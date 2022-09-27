fcf39e26f6f51042d73bec226a108c9916dbc3b6
Владимир Машир <v.mashir@digitalms.ru>
2022-09-27 23:55:10 +0300

    upd schema

--- scripts/migrate/store.up.sql
+++ scripts/migrate/store.up.sql
@@ -2,53 +2,60 @@ BEGIN;
 
 create table if not exists store.user
 (
-    id integer primary key not null,
+    id integer not null AUTO_INCREMENT,
     email text unique not null,
     password text  not null,
-    role text not null
+    role text not null,
+    primary key(id)
 );
 
 create table if not exists store.basket
 (
-    id integer primary key not null,
-    user_id text not null
+    id integer not null AUTO_INCREMENT,
+    user_id text not null,
+    primary key(id)
 );
 
 create table if not exists store.basket_product
 (
-    id integer primary key not null,
+    id integer not null AUTO_INCREMENT,
     product_id text not null,
-    basket_id text not null
+    basket_id text not null,
+    primary key(id)
 );
 
 create table if not exists store.product
 (
-    id integer not null,
+    id integer not null AUTO_INCREMENT,
     name text unique not null,
     price real not null,
     img text not null,
     type_id text not null,
-    brand_id text not null
+    brand_id text not null,
+    primary key(id)
 );
 
 create table if not exists store.product_info
 (
-    id integer not null,
+    id integer not null AUTO_INCREMENT,
     product_id text not null,
     title text not null,
     description text not null,
+    primary key(id)
 );
 
 create table if not exists store.type
 (
-    id integer not null,
-    name text not null
+    id integer not null AUTO_INCREMENT,
+    name text not null,
+    primary key(id)
 );
 
 create table if not exists store.brand
 (
-    id integer not null,
-    name text not null
+    id integer not null AUTO_INCREMENT,
+    name text not null,
+    primary key(id)
 );
 
 
