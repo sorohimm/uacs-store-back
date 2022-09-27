954c1253c8777f5df726fa122357d5c327f17c52
Владимир Машир <v.mashir@digitalms.ru>
2022-09-27 16:03:35 +0300

    store logger, config

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/sorohimm/shop
 
 go 1.18
 
-require github.com/oklog/run v1.1.0
+require (
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/oklog/run v1.1.0
+	go.uber.org/zap v1.23.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+)
--- internal/service/store/service.go
+++ internal/service/store/service.go
@@ -2,7 +2,14 @@ package store
 
 import (
 	"context"
+	"errors"
 	"github.com/sorohimm/shop/internal"
+	"github.com/sorohimm/shop/internal/conf"
+	"github.com/sorohimm/shop/internal/log"
+	"github.com/sorohimm/shop/internal/service/store/config"
+	"os"
+
+	stdl "log"
 )
 
 func NewService() *Service {
@@ -13,6 +20,30 @@ func NewService() *Service {
 
 type Service struct {
 	*internal.RunGroup
+}
+
+func (o *Service) initConfigs(ctx context.Context) context.Context {
+	appConf := &config.Config{}
+	if err := conf.New(appConf); err != nil {
+		if errors.Is(err, conf.ErrHelp) {
+			os.Exit(0)
+		}
+		stdl.Fatalf("failed to read app config: %v", err)
+	}
+	return config.WithContext(ctx, appConf)
+}
+
+func (o *Service) initLogger(ctx context.Context, version, built, appName string) context.Context {
+	appConf := config.FromContext(ctx)
+	// init logger
+	l, err := log.NewZap(
+		appConf.Log.Level,
+		appConf.Log.EncType)
+	if err != nil {
+		stdl.Fatal(err)
+	}
+	logger := l.Sugar().With("v", version, "built", built, "app", appName)
+	return log.CtxWithLogger(ctx, logger.Desugar())
 }
 
 func (o *Service) Init(ctx context.Context, appName, version, built string) {
